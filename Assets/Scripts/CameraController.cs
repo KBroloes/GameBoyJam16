a29bedc4503af07f93633172e7932652b2b24c4b
Kevin Broløs <kevin.broloes@gmail.com>
2016-10-09 18:09:03 +0200

    Add zoom functionality (C to zoom)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public Canvas gameBoyCanvas;
+    public Canvas ZoomedCanvas;
+    
+    void Start()
+    {
+        ZoomedCanvas.enabled = false;
+        gameBoyCanvas.enabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ZoomedCanvas.enabled = !ZoomedCanvas.enabled;
+            gameBoyCanvas.enabled = !ZoomedCanvas.enabled;
+        }
+    }
+}
